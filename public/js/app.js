0c71f7d4433feb7e46b195ac8af192910596bbad
mikey-pawell <mikeypawell@gmail.com>
2015-10-26 15:07:59 -0700

    starting server side

--- public/js/app.js
+++ public/js/app.js
@@ -51,4 +51,35 @@ $(document).ready(function(){
 		return loser;
 
 	});
+//sign-up form
+
+$('#signup-form').on('submit', function(e) {
+    e.preventDefault();
+
+    // select the form and serialize its data
+    var signupData = $("#signup-form").serialize();
+    console.log(signupData);
+    // send POST request to /users with the form data
+    $.post('/users', signupData, function(response) {
+      console.log(response);
+    });
+  });
+
+//login-form
+  $('#login-form').on('submit', function(e) {
+    e.preventDefault();
+
+    // select the form and serialize its data
+    // note: this is the form because the event handler
+    //   was triggered from the form
+    var loginData = $(this).serialize();
+    // send POST request to /login with the form data
+    $.post('/login', loginData, function(response) {
+      console.log(response);
+    });
+  });
+
+
+
+
 });

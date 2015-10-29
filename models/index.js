460fa2082ee0e5da36045cb4bb347b5af3078eb2
mikey-pawell <mikeypawell@gmail.com>
2015-10-29 16:57:55 -0700

    heroku stuff

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require("mongoose");
 
 mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL || 
-                  "mongodb://localhost/project1");
+                  "mongodb://localhost/my_heroku_app");
 
 module.exports.Summary = require('./game');
 module.exports.User = require('./user');
